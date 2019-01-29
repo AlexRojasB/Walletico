c1fb0bcb80cadbe77a8486ab0d8a96e1dc611791
Alexander Rojas <alexrrojas.b@gmail.com>
2019-01-28 20:47:34 -0600

    CurrentBalance Header design

--- Walletico/Walletico/App.xaml.cs
+++ Walletico/Walletico/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Walletico.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace Walletico
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new DetailListPage());
         }
 
         protected override void OnStart()
--- Walletico/Walletico/Pages/DetailListPage.xaml.cs
+++ Walletico/Walletico/Pages/DetailListPage.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace Walletico
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Walletico.Pages
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailListPage : ContentPage
     {
-        public MainPage()
+        public DetailListPage()
         {
             InitializeComponent();
         }
