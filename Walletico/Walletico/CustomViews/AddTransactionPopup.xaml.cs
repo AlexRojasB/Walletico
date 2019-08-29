618d667cbda2855fa672db4063a21888151eee6a
Alexander Rojas <alexrrojas.b@gmail.com>
2019-08-28 20:32:48 -0600

    First Popup section

---
+++ Walletico/Walletico/CustomViews/AddTransactionPopup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Walletico.CustomViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddTransactionPopup : ContentView
+    {
+        public delegate void ClickExpandDelegate();
+        public AddTransactionPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void IncomeFirstSectionTapped(object sender, EventArgs e)
+        {
+
+        }
+
+        public ClickExpandDelegate OnExpandTapped { get; set; }
+        public double IncomeFirstSectionHeigh => this.FirstSection.Height;
+    }
+}
--- Walletico/Walletico/Pages/DetailListPage.xaml.cs
+++ Walletico/Walletico/Pages/DetailListPage.xaml.cs
@@ -6,9 +6,45 @@ namespace Walletico.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailListPage : ContentPage
     {
+
+        const uint AnimationSpeed = 300;
         public DetailListPage()
         {
             InitializeComponent();
+        }
+
+        private async void PageFader_Tapped(object sender, System.EventArgs e)
+        {
+            await AddTransPopup.TranslateTo(0, Height, AnimationSpeed, Easing.SinInOut);
+            await PageFader.FadeTo(0, AnimationSpeed, Easing.SinInOut);
+            PageFader.IsVisible = false;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.AddTransPopup.OnExpandTapped += ExpandPopup;
+        }
+
+        private void ExpandPopup()
+        {
+            var height = this.AddTransPopup.Height;
+            this.AddTransPopup.TranslateTo(0, this.Height - height, AnimationSpeed, Easing.SinInOut);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            this.AddTransPopup.OnExpandTapped -= ExpandPopup;
+        }
+
+        private async void IncomeTotalTapped(object sender, System.EventArgs e)
+        {
+            var pageHeight = Height;
+            var firstSection = AddTransPopup.IncomeFirstSectionHeigh;
+            PageFader.IsVisible = true;
+            await PageFader.FadeTo(1, AnimationSpeed, Easing.SinInOut);
+            await AddTransPopup.TranslateTo(0, pageHeight - firstSection, AnimationSpeed, Easing.SinInOut);
         }
     }
 }
